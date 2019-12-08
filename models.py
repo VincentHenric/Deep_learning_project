1f1209db318a6c25e19f7a966edec9d5faf0b8d3
HENRIC <vincent.henric@hotmail.fr>
2019-12-08 16:59:15 +0100

    add model code for week 2

---
+++ models.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 18:34:48 2019
+
+@author: henric
+"""
+import tensorflow.keras as keras
+from tensorflow.keras import layers 
+
+planes = 8
+moves = 361
+
+def get_model_baseline():
+    input = keras.Input(shape=(19, 19, planes), name='board')
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(input)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(x)
+    policy_head = layers.Conv2D(1, 3, activation='relu', padding='same')(x)
+    policy_head = layers.Flatten()(policy_head)
+    policy_head = layers.Dense(moves, activation='softmax', name='policy')(policy_head)
+    value_head = layers.Flatten()(x)
+    value_head = layers.Dense(1, activation='sigmoid', name='value')(value_head)
+    
+    model = keras.Model(inputs=input, outputs=[policy_head, value_head])
+    return model
+
+def get_model_week2(convol_size=128):
+    input = keras.Input(shape=(19, 19, planes), name='board')
+    x = layers.Conv2D(convol_size, 3, padding='valid')(input)
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+    
+    y = layers.Conv2D(convol_size, 3, padding='same')(x)
+    y = layers.BatchNormalization(axis=-1)(y)
+    y = layers.Activation('relu')(y)
+    y = layers.Conv2D(convol_size, 3, padding='same')(y)
+    y = layers.BatchNormalization(axis=-1)(y)
+    x = layers.Add()([x,y])
+    x = layers.Activation('relu')(x)
+    
+    y = layers.Conv2D(convol_size, 3, padding='same')(x)
+    y = layers.BatchNormalization(axis=-1)(y)
+    y = layers.Activation('relu')(y)
+    y = layers.Conv2D(convol_size, 3, padding='same')(y)
+    y = layers.BatchNormalization(axis=-1)(y)
+    x = layers.Add()([x,y])
+    x = layers.Activation('relu')(x)
+    
+    policy_head = layers.Conv2D(2, 1)(x)
+    policy_head = layers.BatchNormalization(axis=-1)(policy_head)
+    policy_head = layers.Activation('relu')(policy_head)
+    policy_head = layers.Flatten()(policy_head)
+    policy_head = layers.Dense(moves, activation='softmax', name='policy')(policy_head)
+    
+    value_head = layers.Conv2D(1, 1)(x)
+    value_head = layers.BatchNormalization(axis=-1)(value_head)
+    value_head = layers.Activation('relu')(value_head)
+    value_head = layers.Flatten()(value_head)
+    value_head = layers.Dense(128, activation='relu')(value_head)
+    value_head = layers.Dense(1, activation='sigmoid', name='value')(value_head)
+    
+    model = keras.Model(inputs=input, outputs=[policy_head, value_head])
+    return model
+    
+    
+    
+    
+    
